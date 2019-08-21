dee6b67d9372e39582cdfb9d6fc6533834f0ed2b
fstenneler <fabien.stenneler@gmail.com>
2019-08-21 14:55:25 +0200

    added a default config.php file with raw fields

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+ 
+  /**
+   * Constantes de configuration de l'application
+   **/
+ 
+  //Serveur BDD
+  define('DB_SERVER', '');
+ 
+  //User BDD
+  define('DB_USER','');
+ 
+  //Password BDD
+  define('DB_PASSWORD','');
+ 
+  //Nom BDD
+  define('DB_NAME','webdev-blog');
+ 
+  //Nombre de posts à afficher par page
+  define('POST_NUMBER', 8);
+ 
+  //Taille maximum des images à charger
+  define('MAX_FILE_SIZE', 500000);
+
+  //Dossier de stockage des images des articles
+  define('GALLERY_DIR', '/public/front/images/gallery/');
